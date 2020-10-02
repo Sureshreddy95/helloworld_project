d8f809eed724451cd387c97a37044b7bdd349687
Sureshreddy95 <Sureshreddy1995@gmail.com>
2020-10-02 11:28:00 +0530

    Adding new Function substruction

--- calc.py
+++ calc.py
@@ -1,8 +1,11 @@
 # Function to add two numbers 
 def add(num1, num2): 
 	return num1 + num2
+def sub(num1, num2): 
+	return num1 + num2
 
 number_1 = int(input("Enter first number: ")) 
 number_2 = int(input("Enter second number: ")) 
 print(number_1, "+", number_2, "=", add(number_1, number_2))
+print(number_1, "-", number_2, "=", sub(number_1, number_2))
 
